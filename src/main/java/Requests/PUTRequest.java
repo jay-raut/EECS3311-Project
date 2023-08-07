737c845a2b4fb2fe333f4fd57a21a4090fe442e2
jay-raut <jayraut@my.yorku.ca>
2023-08-06 22:51:41 -0400

    Implemented addRelationship method

--- src/main/java/Requests/PUTRequest.java
+++ src/main/java/Requests/PUTRequest.java
@@ -11,6 +11,8 @@ import java.util.Map;
 import org.neo4j.driver.v1.*;
 import org.neo4j.driver.v1.Record;
 
+import javax.swing.plaf.nimbus.State;
+
 class PUTRequest extends AbstractRequest {
     private interface EndpointHandler {
         boolean handleEndpoint(Map<String, String> requestQuery);
@@ -96,12 +98,12 @@ class PUTRequest extends AbstractRequest {
         }
 
         String name = requestQuery.get("name");
-        String actorId = requestQuery.get("movieId");
+        String movieId = requestQuery.get("movieId");
 
         Driver driver = Neo4jDriverSession.getDriverInstance();
         try (Session session = driver.session()) { //checking if id already exists
-            String checkForExistingActor = "MATCH (a:Movie {movieId: $movieId}) RETURN COUNT(a) AS count";
-            StatementResult result = session.run(checkForExistingActor, Values.parameters("movieId", actorId));
+            String checkForExistingMovie = "MATCH (a:Movie {movieId: $movieId}) RETURN COUNT(a) AS count";
+            StatementResult result = session.run(checkForExistingMovie, Values.parameters("movieId", movieId));
             if (result.hasNext()) {
                 Record record = result.next();
                 int count = record.get("count").asInt();
@@ -112,7 +114,7 @@ class PUTRequest extends AbstractRequest {
 
             Session newSession = driver.session();
             String createActorQuery = "CREATE (:Movie {name: $name, movieId: $movieId})";
-            newSession.run(createActorQuery, Values.parameters("name", name, "movieId", actorId));
+            newSession.run(createActorQuery, Values.parameters("name", name, "movieId", movieId));
 
 
         } catch (Exception e) {
@@ -125,6 +127,46 @@ class PUTRequest extends AbstractRequest {
 
     private static boolean addRelationship(Map<String, String> requestQuery) {
         System.out.println("Called addRelationship");
+        if (requestQuery.size() != 2 || !requestQuery.containsKey("actorId") || !requestQuery.containsKey("movieId")) {
+            return false;
+        }
+        Driver driver = Neo4jDriverSession.getDriverInstance();
+        String actorId = requestQuery.get("actorId");
+        String movieId = requestQuery.get("movieId");
+        try (Session session = driver.session()) {
+            String checkForExistingMovie = "MATCH (a:Movie {movieId: $movieId}) RETURN COUNT(a) AS count";
+            String checkForExistingActor = "MATCH (a:Actor {actorId: $actorId}) RETURN COUNT(a) AS count";
+            StatementResult databaseContainsMovie = session.run(checkForExistingMovie, Values.parameters("movieId", movieId));
+            if (databaseContainsMovie.hasNext()) { //checking if the database contains the movie
+                Record record = databaseContainsMovie.next();
+                int count = record.get("count").asInt();
+                if (count != 1) {//must be 1 only
+                    return false;
+                }
+            }
+            Session newSession = driver.session();
+            StatementResult databaseContainsActor = newSession.run(checkForExistingActor, Values.parameters("actorId", actorId));
+            if (databaseContainsActor.hasNext()) {//checking if the database contains the actor
+                Record record = databaseContainsActor.next();
+                int count = record.get("count").asInt();
+                if (count != 1) {//must be 1 only
+                    return false;
+                }
+            }
+
+            Session checkIfRelationshipExists = driver.session(); //check if the relationship already exists
+            String checkRelationshipExists = "MATCH (a:Actor {actorId: $actorId}), (m: Movie {movieId: $movieId}) RETURN EXISTS ((a)-[:ACTED_IN]->(m)) AS relationshipExists";
+            StatementResult result = checkIfRelationshipExists.run(checkRelationshipExists, Values.parameters("actorId", actorId, "movieId", movieId));
+            if (result.hasNext()) {
+                if (result.next().get("relationshipExists").asBoolean()) {
+                    return false;
+                }
+            }
+
+            Session connectNodesSession = driver.session(); //create the relationship
+            String createRelationship = "MATCH (a:Actor {actorId: $actorId}), (m: Movie {movieId: $movieId}) CREATE (a)-[:ACTED_IN]->(m)";
+            connectNodesSession.run(createRelationship, Values.parameters("actorId", actorId, "movieId", movieId));
+        }
         return true;
     }
 }
